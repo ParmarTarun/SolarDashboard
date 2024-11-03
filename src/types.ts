8fbc1bdd81f12b9dc3f274df249cff1378032b4b
ParmarTarun <ParmarTarun.dev@gmail.com>
2024-11-03 13:47:32 -0500

    resolve a minor bug

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,3 @@
-import { FieldErrors, UseFormRegister } from "react-hook-form";
-import { FormDataSchema } from "./lib/schemas/QuoteForm";
-import { z } from "zod";
-
 export type Message = {
   id: number;
   text: string;
@@ -12,11 +8,6 @@ export type formStep = {
   id: string;
   name: string;
   fields?: string[];
-};
-
-export type formStepComponentProps = {
-  register: UseFormRegister<z.infer<typeof FormDataSchema>>;
-  errors: FieldErrors<z.infer<typeof FormDataSchema>>;
 };
 
 export type powerType =
